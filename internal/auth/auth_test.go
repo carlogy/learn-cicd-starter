3ce1e85733b8f7a95c4aea0f576b43697f563e2e
Carlo Yannuzzi <109364007+carlogy@users.noreply.github.com>
2025-07-17 10:54:06 -0400

    updated README (#1)
    * updated README
    
    * added .github/workflows direcotries and ci.yml workflow
    
    * updated ci.yml workflow
    
    * updated last step in workflow
    
    * added tests step to test workflow and unit tests for GetApiKey function
    
    * updated forced failed test to run correctly
    
    * updated readme with test status

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,39 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestValidAPIKEY(t *testing.T) {
+	header := http.Header{}
+	header.Add("Authorization", "ApiKey 1234")
+
+	got, err := GetAPIKey(header)
+
+	if got != "1234" || err != nil {
+		t.Errorf("Error getting API Key\t Received%v\n", got)
+	}
+}
+
+func TestInvalidAPIKey(t *testing.T) {
+	header := http.Header{}
+	header.Add("Authorization", " Invalid ApiKey")
+
+	_, err := GetAPIKey(header)
+
+	if err.Error() != "malformed authorization header" {
+		t.Errorf("Received unexpected err:\t%v", err.Error())
+	}
+}
+
+func TestNoAuthHeader(t *testing.T) {
+	header := http.Header{}
+	header.Add("header1", "Cool Header")
+
+	_, err := GetAPIKey(header)
+
+	if err.Error() != "no authorization header included" {
+		t.Errorf("Received unexpected err:\t%v", err.Error())
+	}
+}
